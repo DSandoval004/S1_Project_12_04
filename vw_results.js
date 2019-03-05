075373a605c0b3583aab85692d4c6e63ed168006
Diego Sandoval <dsandoval004@west-mec.org>
2019-03-05 14:12:05 -0700

    Percent and comments
    Fixed it so it would actually show the percent bars, and  added comments.

--- vw_results.js
+++ vw_results.js
@@ -23,61 +23,77 @@
    
       
 */
-// DVARS:
+// DVARS: Creates the reportHTML var and gives it HTML for a header
 var reportHTML = `<h1>${raceTitle}</h1>`;
-// DVARN:
-var totalVotes = 0;
-// DLOOP:
+// DLOOP: Creates a loop to run through each of the districts and create a table for them
 for (var i = 0; i < race.length; i++) {
+    // DVARN: stores the total votes for each district
+    var totalVotes = 0;
+    // DDOES: Calculates the total votes from the votes array
     votes[i].forEach(calcSum);
+    // DVARS: Adds to the reportHTML variable the tables for each district
     reportHTML += "<table>";
     reportHTML += `<caption>${race[i]}</caption>`;
     reportHTML += "<tr><th>Canidate</th><th>Votes</th></tr>";
     reportHTML += canidateRows(i, totalVotes);
     reportHTML += "</table>";
 }
-// DDOES:
+// DDOES: Applies the reportHTML to the document
 document.getElementById('reportHTML').innerHTML = reportHTML;
-// DFUNC:
+// DFUNC: create the function to create the rows of the tables
 function canidateRows(raceNum, totalVotes) {
-    // DVARS:
+    // DVARS: Creates the rowHTML variable
     var rowHTML = "";
+    // DLOOP: Runs 3 times cause their is 3 canadites for each district
     for (var j = 0; j <= 2; j++) {
-        // DVARL:
+        // DVARL: Creates some local variable by calling upon the arrays, or a function
         var candidateName = candidate[raceNum][j];
         var canidateParty = party[raceNum][j];
         var candidateVotes = votes[raceNum][j];
         var candidatePercent = calcPercent(candidateVotes, totalVotes);
-        // DVARS:
-        rowHTML += `<tr><td>${candidateName}${canidateParty}</td><td>${candidateVotes.toLocaleString()}${candidatePercent.toFixed(1)}</td>`
-        for (var k = 0; k < candidatePercent; k++) {
-            createBar(canidateParty)
+        // DVARS: Adds to the rowHTML var
+        rowHTML += `<tr><td>${candidateName} (${canidateParty})</td><td>${candidateVotes.toLocaleString()} (${candidatePercent.toFixed(1)}%)</td>`
+        // DLOOP: Creates a loop the adds a visual of the oercent of the canidate
+        for (var k = 0; k <= candidatePercent; k++) {
+            // DVARS: Calls upon the createBar function with the parameter of canidateParty and adds it to rowHTML
+            rowHTML += createBar(canidateParty)
         }
+        // DVARS: Closing row tag
         rowHTML += "</tr>";
     }
-    // DDOES:
+    // DDOES: Returns the value of rowHTML
     return rowHTML;
 }
-
+// DFUNC: The createBar function
 function createBar(partyType) {
+    // DVARS: the barHTML variable
     var barHTML = "";
+    // DSWCA: Determines what party they belong to, then adds to the barHTML the td with its respective class
     switch (partyType) {
         case "D":
             barHTML = "<td class='dem'></td>"
             break;
         case "R":
-            barHTML = "<td class='dem'></td>"
+            barHTML = "<td class='rep'></td>"
             break;
         case "I":
             barHTML = "<td class='ind'></td>"
             break;
     }
+    // DDOES: Returns the barHTML
     return barHTML;
 }
 
-
-
-
+/*
+ *
+ *   ____      _ _ _                _      _____                 _   _                 
+ *  / ___|__ _| | | |__   __ _  ___| | __ |  ___|   _ _ __   ___| |_(_) ___  _ __  ___ 
+ * | |   / _` | | | '_ \ / _` |/ __| |/ / | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+ * | |__| (_| | | | |_) | (_| | (__|   <  |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
+ *  \____\__,_|_|_|_.__/ \__,_|\___|_|\_\ |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+ *                                                                                     
+ *
+ */
 
 /* Callback Function to calculate an array sum */
 function calcSum(value) {
